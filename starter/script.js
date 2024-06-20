d3a2e0b76ef618891eb3cd450aecf0ed65b9ba7e
Saodat <saodat.riskiyeva@gmail.com>
2024-06-20 07:55:13 -0400

    constructor functiond and the new operator

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  console.log(this);
+
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+const matilda = new Person('Matilda', 2015);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person);
+console.log(jonas);
