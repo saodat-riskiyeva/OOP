576b073c6f2136796019aaa8ca3b86582eb69b9c
Saodat <saodat.riskiyeva@gmail.com>
2024-06-21 07:49:46 -0400

    inheritance of prototypes

--- starter/script.js
+++ starter/script.js
@@ -11,7 +11,7 @@ const Person = function (firstName, birthYear) {
 };
 
 const jonas = new Person('Jonas', 1991);
-console.log(jonas);
+// console.log(jonas);
 
 const matilda = new Person('Matilda', 2015);
 const jack = new Person('Jack', 1975);
@@ -22,13 +22,13 @@ const jack = new Person('Jack', 1975);
 
 // console.log(Person.prototype);
 
-// Person.prototype.calcAge = function () {
-//   console.log(2037 - this.birthYear);
-// };
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
 
-// jonas.calcAge();
-// matilda.calcAge();
-// jack.calcAge();
+jonas.calcAge();
+matilda.calcAge();
+jack.calcAge();
 
 // console.log(jonas.__proto__);
 // console.log(jonas.__proto__ === Person.prototype);
@@ -43,3 +43,24 @@ Person.prototype.species = 'Homo Sapiens';
 
 // console.log(matilda.hasOwnProperty('firstName'));
 // console.log(matilda.hasOwnProperty('species'));
+
+// console.log(jonas.__proto__);
+// // Object.prototype - top of prototype chain
+// console.log(jonas.__proto__.__proto__);
+// console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 5, 6, 9, 3, 5, 6, 9];
+// console.log(arr.__proto__);
+// console.log(arr.__proto__ === Array.prototype);
+
+// console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
