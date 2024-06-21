2d336cd7d365c20432bb2d8e4993cd1e354aaa07
Saodat <saodat.riskiyeva@gmail.com>
2024-06-21 08:30:05 -0400

    getters and setters

--- starter/script.js
+++ starter/script.js
@@ -70,24 +70,58 @@ const arr = [3, 5, 6, 9, 3, 5, 6, 9];
 
 // }
 
-// class declaraion
+// // class declaraion
 class PersonCl {
-  constructor(firstName, birthYear) {
-    this.firstName = firstName;
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
     this.birthYear = birthYear;
   }
   calcAge() {
     console.log(2037 - this.birthYear);
   }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name!`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
 }
 
-const jessica = new PersonCl('Jessica', 1996);
-console.log(jessica);
+const jessica = new PersonCl('Jessica Davis', 1996);
 
-jessica.calcAge();
+// jessica.calcAge();
+
+console.log(jessica.age);
+
+console.log(jessica);
 
 PersonCl.prototype.greet = function () {
   console.log(`Hey ${this.firstName}`);
 };
 
 jessica.greet();
+
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
