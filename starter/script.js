7f637a9b6896c5c8b1360450c592c2accda434b8
Saodat <saodat.riskiyeva@gmail.com>
2024-06-21 07:32:48 -0400

    prototypes

--- starter/script.js
+++ starter/script.js
@@ -15,31 +15,31 @@ console.log(jonas);
 
 const matilda = new Person('Matilda', 2015);
 const jack = new Person('Jack', 1975);
-console.log(matilda, jack);
+// console.log(matilda, jack);
 
-console.log(jonas instanceof Person);
-console.log(jonas);
+// console.log(jonas instanceof Person);
+// console.log(jonas);
 
-console.log(Person.prototype);
+// console.log(Person.prototype);
 
-Person.prototype.calcAge = function () {
-  console.log(2037 - this.birthYear);
-};
+// Person.prototype.calcAge = function () {
+//   console.log(2037 - this.birthYear);
+// };
 
-jonas.calcAge();
-matilda.calcAge();
-jack.calcAge();
+// jonas.calcAge();
+// matilda.calcAge();
+// jack.calcAge();
 
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype);
 
-console.log(Person.prototype.isPrototypeOf(jonas));
-console.log(Person.prototype.isPrototypeOf(matilda));
-console.log(Person.prototype.isPrototypeOf(Person));
+// console.log(Person.prototype.isPrototypeOf(jonas));
+// console.log(Person.prototype.isPrototypeOf(matilda));
+// console.log(Person.prototype.isPrototypeOf(Person));
 
 Person.prototype.species = 'Homo Sapiens';
-console.log(jonas.species);
-console.log(matilda);
+// console.log(jonas.species);
+// console.log(matilda);
 
-console.log(matilda.hasOwnProperty('firstName'));
-console.log(matilda.hasOwnProperty('species'));
+// console.log(matilda.hasOwnProperty('firstName'));
+// console.log(matilda.hasOwnProperty('species'));
